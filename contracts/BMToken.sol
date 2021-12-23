8b067e5884009c39c620f84993a91d0d5d8ae867
bmstar <bmstar2012@gmail.com>
2021-12-23 15:26:17 +0800

    added token

--- contracts/BMToken.sol
+++ contracts/BMToken.sol
@@ -2,9 +2,18 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract BMToken is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, 100000 * (10 ** 18));
+import "./token/behaviours/ERC20Mintable.sol";
+
+contract BMToken is ERC20Burnable, ERC20Mintable, Ownable {
+    constructor(string memory name, string memory symbol, uint256 initialBalance) ERC20(name, symbol) {
+        require(initialBalance > 0, "BMToken: Supply cannot be zero");
+        _mint(msg.sender, initialBalance * (10 ** 18));
+    }
+
+    function _mint(address account, uint256 amount) internal override onlyOwner {
+        super._mint(account, amount);
     }
 }
